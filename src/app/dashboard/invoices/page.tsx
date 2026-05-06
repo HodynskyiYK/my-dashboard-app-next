d005dd3f24ad1f3568adbbcf70e839ca3bd631af
HodynskyiYK <hodynskyiyk@gmail.com>
2026-05-06 20:08:49 +0200

    Added pages and navigation

---
+++ src/app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+function Invoices() {
+  return (
+    <div className="flex-1 p-4">
+      <h1 className="text-2xl font-bold mb-4">Invoices</h1>
+      <p>Here you can manage your invoices and view their details.</p>
+    </div>
+  );
+}
+
+export default Invoices;
